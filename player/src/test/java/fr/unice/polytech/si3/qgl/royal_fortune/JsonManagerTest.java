edbbfe8c43f399828c874c88eb25167cabccde48
AyoubIMAMI <youbima17@gmail.com>
2022-02-01 15:12:00 +0100

    Tests

--- player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/JsonManagerTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/JsonManagerTest.java
@@ -34,7 +34,7 @@ public class JsonManagerTest {
                 },
                 "name": "Boat test"
             }""";
-        Ship ship = JsonManager.readJson(json);
+        Ship ship = (Ship) JsonManager.readJson(json, "ship");
 
         assertEquals("ship", ship.getType());
         assertEquals(100, ship.getLife());
@@ -55,7 +55,35 @@ public class JsonManagerTest {
 
     @Test
     void getNodeTest() throws Exception {
-        String json = "\"ship\": {\n" +
+        String json = "{\n" +
+                "  \"goal\": {\n" +
+                "    \"mode\": \"REGATTA\",\n" +
+                "    \"checkpoints\": [\n" +
+                "      {\n" +
+                "        \"position\": {\n" +
+                "          \"x\": 1000,\n" +
+                "          \"y\": 0,\n" +
+                "          \"orientation\": 0\n" +
+                "        },\n" +
+                "        \"shape\": {\n" +
+                "          \"type\": \"circle\",\n" +
+                "          \"radius\": 50\n" +
+                "        }\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"position\": {\n" +
+                "          \"x\": 0,\n" +
+                "          \"y\": 0,\n" +
+                "          \"orientation\": 0\n" +
+                "        },\n" +
+                "        \"shape\": {\n" +
+                "          \"type\": \"circle\",\n" +
+                "          \"radius\": 50\n" +
+                "        }\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  \"ship\": {\n" +
                 "    \"type\": \"ship\",\n" +
                 "    \"life\": 100,\n" +
                 "    \"position\": {\n" +
@@ -67,17 +95,103 @@ public class JsonManagerTest {
                 "    \"deck\": {\n" +
                 "      \"width\": 3,\n" +
                 "      \"length\": 6\n" +
-                "    }";
+                "    },\n" +
+                "    \"entities\": [\n" +
+                "      {\n" +
+                "        \"x\": 1,\n" +
+                "        \"y\": 0,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 1,\n" +
+                "        \"y\": 2,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 3,\n" +
+                "        \"y\": 0,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 3,\n" +
+                "        \"y\": 2,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 4,\n" +
+                "        \"y\": 0,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 4,\n" +
+                "        \"y\": 2,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 2,\n" +
+                "        \"y\": 1,\n" +
+                "        \"type\": \"sail\",\n" +
+                "        \"openned\": false\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 5,\n" +
+                "        \"y\": 0,\n" +
+                "        \"type\": \"rudder\"\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"shape\": {\n" +
+                "      \"type\": \"rectangle\",\n" +
+                "      \"width\": 3,\n" +
+                "      \"height\": 6,\n" +
+                "      \"orientation\": 0\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"sailors\": [\n" +
+                "    {\n" +
+                "      \"x\": 0,\n" +
+                "      \"y\": 0,\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"Edward Teach\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"x\": 0,\n" +
+                "      \"y\": 1,\n" +
+                "      \"id\": 1,\n" +
+                "      \"name\": \"Edward Pouce\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"x\": 0,\n" +
+                "      \"y\": 2,\n" +
+                "      \"id\": 2,\n" +
+                "      \"name\": \"Tom Pouce\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"x\": 1,\n" +
+                "      \"y\": 0,\n" +
+                "      \"id\": 3,\n" +
+                "      \"name\": \"Jack Teach\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"x\": 1,\n" +
+                "      \"y\": 1,\n" +
+                "      \"id\": 4,\n" +
+                "      \"name\": \"Jack Teach\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"x\": 1,\n" +
+                "      \"y\": 2,\n" +
+                "      \"id\": 5,\n" +
+                "      \"name\": \"Tom Pouce\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"shipCount\": 1\n" +
+                "}";
 
-//        String result = "{\n" +
-//        "      \"x\": 0,\n" +
-//                "      \"y\": 0,\n" +
-//                "      \"orientation\": 0\n" +
-//                "    }";
+        String result = "{\"type\":\"ship\",\"life\":100,\"position\":{\"x\":0,\"y\":0,\"orientation\":0},\"name\":\"Les copaings d'abord!\",\"deck\":{\"width\":3,\"length\":6},\"entities\":[{\"x\":1,\"y\":0,\"type\":\"oar\"},{\"x\":1,\"y\":2,\"type\":\"oar\"},{\"x\":3,\"y\":0,\"type\":\"oar\"},{\"x\":3,\"y\":2,\"type\":\"oar\"},{\"x\":4,\"y\":0,\"type\":\"oar\"},{\"x\":4,\"y\":2,\"type\":\"oar\"},{\"x\":2,\"y\":1,\"type\":\"sail\",\"openned\":false},{\"x\":5,\"y\":0,\"type\":\"rudder\"}],\"shape\":{\"type\":\"rectangle\",\"width\":3,\"height\":6,\"orientation\":0}}";
 
-        String result = "100";
+//        String result = "100";
 
-        assertEquals(result,jsonManager.getNode(json, "life"));
+        assertEquals(result,jsonManager.getNode(json, "ship"));
     }
 
 
