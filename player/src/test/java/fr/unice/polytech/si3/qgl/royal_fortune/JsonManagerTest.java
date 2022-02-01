d3ec5ea46acbacdfcb6e24204b51df636b48991a
AyoubIMAMI <youbima17@gmail.com>
2022-02-01 13:25:26 +0100

    Json method + tests

--- player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/JsonManagerTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/JsonManagerTest.java
@@ -1,7 +1,10 @@
 package fr.unice.polytech.si3.qgl.royal_fortune;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonManagerTest {
 
@@ -14,4 +17,14 @@ public class JsonManagerTest {
     void readJsonTest(){
         String json = "";
     }
+
+    @Test
+    void writeJsonActionTest() throws JsonProcessingException {
+        JsonManager jsonManager = new JsonManager();
+        String actionDone = "[{\"sailorId\":0,\"type\":\"OAR\"},{\"sailorId\":1,\"type\":\"OAR\"}]";
+
+        assertEquals(actionDone,jsonManager.writeJsonAction());
+    }
+
+    
 }
