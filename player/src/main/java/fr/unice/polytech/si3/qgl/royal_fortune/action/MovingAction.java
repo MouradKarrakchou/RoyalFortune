7e3a1e5cb48930609536d7bebe8b3823ba7c7ca4
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-02-05 15:27:31 +0100

    Adding JSON conversion for Moving action

--- player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/action/MovingAction.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/action/MovingAction.java
@@ -1,5 +1,8 @@
 package fr.unice.polytech.si3.qgl.royal_fortune.action;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import fr.unice.polytech.si3.qgl.royal_fortune.Sailor;
 
 public class MovingAction extends Action {
@@ -10,5 +13,22 @@ public class MovingAction extends Action {
         super(sailor, "MOVING");
         this.xDistance = xDistance;
         this.yDistance = yDistance;
+    }
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode oarActionJSON = mapper.createObjectNode();
+        oarActionJSON.put("id", sailorId);
+        oarActionJSON.put("type", "MOVING");
+        oarActionJSON.put("xdistance", xDistance);
+        oarActionJSON.put("ydistance", yDistance);
+
+        try {
+            return mapper.writeValueAsString(oarActionJSON);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
