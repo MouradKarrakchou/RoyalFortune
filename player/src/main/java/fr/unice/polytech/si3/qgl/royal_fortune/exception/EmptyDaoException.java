429b83d512e9af59c55c4423b59b1b6ee02e969a
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-03-06 18:18:34 +0100

    end of mutation tests fix #62

---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/exception/EmptyDaoException.java
@@ -0,0 +1,7 @@
+package fr.unice.polytech.si3.qgl.royal_fortune.exception;
+
+public class EmptyDaoException extends Exception    {
+    public EmptyDaoException(String errorMessage) {
+        super(errorMessage);
+    }
+}
