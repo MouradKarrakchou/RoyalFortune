2f3605778e6b04c479780745fe619fa056e5c429
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-03-08 19:59:11 +0100

    push

--- tooling/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/tooling/Application.java
+++ tooling/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/tooling/Application.java
@@ -6,8 +6,5 @@ import java.util.logging.Logger;
 
 public class Application {
 
-	public static void main(String[] args)
-	{
-		fr.unice.polytech.si3.qgl.soyouz.tooling.Application.runSimulator(fr.unice.polytech.si3.qgl.royal_fortune.Cockpit.class); // <-----------
-	}
+
 }
