416827885cff7f8e286e143b3859d350176e1971
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-03-11 12:54:10 +0100

    checking sailor mouvement #61

---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/target/Beacon.java
@@ -0,0 +1,11 @@
+package fr.unice.polytech.si3.qgl.royal_fortune.target;
+
+import fr.unice.polytech.si3.qgl.royal_fortune.ship.Position;
+import fr.unice.polytech.si3.qgl.royal_fortune.ship.shape.Shape;
+
+public class Beacon extends Checkpoint{
+
+    public Beacon (Position position, Shape shape){
+        super(position, shape);
+    }
+}
---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/target/Goal.java
@@ -0,0 +1,32 @@
+package fr.unice.polytech.si3.qgl.royal_fortune.target;
+
+import fr.unice.polytech.si3.qgl.royal_fortune.target.Checkpoint;
+
+import java.util.List;
+
+public class Goal {
+    String mode;
+    List<Checkpoint> checkpoints;
+
+    public Goal(String mode, List<Checkpoint> checkpoints){
+        this.mode = mode;
+        this.checkpoints = checkpoints;
+    }
+
+    public Goal(){}
+    public void setMode(String mode){
+        this.mode = mode;
+    }
+
+    public Checkpoint getCurrentCheckPoint() {
+        return checkpoints.get(0);
+    }
+
+    public void nextCheckPoint(){checkpoints.remove(0);}
+
+    public void setCheckpoints(List<Checkpoint> checkpoints){
+        this.checkpoints = checkpoints;
+    }
+
+    public List<Checkpoint> getCheckPoints() { return checkpoints; }
+}
