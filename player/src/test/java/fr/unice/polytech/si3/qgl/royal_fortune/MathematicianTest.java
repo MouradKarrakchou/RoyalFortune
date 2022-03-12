4742180d908193cbd8bf6eddfd8710c334524dcf
AyoubIMAMI <youbima17@gmail.com>
2022-03-12 14:18:05 +0100

    Tests Mathematician/changeBase() #68 #69

--- player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/MathematicianTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/MathematicianTest.java
@@ -1,9 +1,21 @@
 package fr.unice.polytech.si3.qgl.royal_fortune;
 
 import fr.unice.polytech.si3.qgl.royal_fortune.calculus.Mathematician;
+import fr.unice.polytech.si3.qgl.royal_fortune.captain.Crewmates.Sailor;
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.shape.Rectangle;
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.shape.Shape;
+import fr.unice.polytech.si3.qgl.royal_fortune.ship.Deck;
 import fr.unice.polytech.si3.qgl.royal_fortune.ship.Position;
+import fr.unice.polytech.si3.qgl.royal_fortune.ship.Ship;
+import fr.unice.polytech.si3.qgl.royal_fortune.ship.entities.Entities;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Math.pow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MathematicianTest {
     @BeforeEach
@@ -13,10 +25,16 @@ public class MathematicianTest {
 
     @Test
     void changeBaseTest() {
-        Position ship = new Position(10, 20, 0);
-        Position objectInShip = new Position(0, 0, 0);
-        Mathematician.changeBase(objectInShip, 2, 3);
+        Position positionInSeaBase = Mathematician.changeBase(new Position(0,0, Math.PI/4), 2, 1);
+        double scale = Math.pow(10, 4);
+        double x = Math.round(positionInSeaBase.getX()*scale)/scale;
+        double y = Math.round(positionInSeaBase.getY()*scale)/scale;
+        double ori = Math.round(positionInSeaBase.getOrientation()*scale)/scale;
 
-        assertEquals(ship.getX(), )
+        Position positionAfterBase = new Position(0.7071, 2.1213, 0);
+
+        assertEquals(positionAfterBase.getX(), x);
+        assertEquals(positionAfterBase.getY(), y);
+        assertEquals(positionAfterBase.getOrientation(), ori);
     }
 }
