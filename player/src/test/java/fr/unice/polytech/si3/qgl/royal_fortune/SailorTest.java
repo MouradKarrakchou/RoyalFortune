04da634951fec21d6836d4dde68ae8c3276f3c47
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-03-20 12:16:06 +0100

    Adding isOnTargetEntityTest

--- player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/SailorTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/SailorTest.java
@@ -72,15 +72,45 @@ class SailorTest {
 		assertTrue(correctSailor.isOnTheTargetEntity(associations));
 
 		Sailor incorrectSailor01 = new Sailor(1, 0,0, "sailor1");
+		associations.dissociateAll();
+		associations.addAssociation(incorrectSailor01, entity);
+		assertFalse(incorrectSailor01.isOnTheTargetEntity(associations));
+
 		Sailor incorrectSailor02 = new Sailor(2, 0,1,  "sailor2");
+		associations.dissociateAll();
+		associations.addAssociation(incorrectSailor02, entity);
+		assertFalse(incorrectSailor02.isOnTheTargetEntity(associations));
+
 		Sailor incorrectSailor03 = new Sailor(3, 0,2,  "sailor3");
+		associations.dissociateAll();
+		associations.addAssociation(incorrectSailor03, entity);
+		assertFalse(incorrectSailor03.isOnTheTargetEntity(associations));
+
 
 		Sailor incorrectSailor04 = new Sailor(4, 1,0,  "sailor4");
+		associations.dissociateAll();
+		associations.addAssociation(incorrectSailor04, entity);
+		assertFalse(incorrectSailor04.isOnTheTargetEntity(associations));
+
 		Sailor incorrectSailor05 = new Sailor(5, 1,2,  "sailor5");
+		associations.dissociateAll();
+		associations.addAssociation(incorrectSailor05, entity);
+		assertFalse(incorrectSailor05.isOnTheTargetEntity(associations));
 
 		Sailor incorrectSailor06 = new Sailor(6, 2,0, "sailor6");
+		associations.dissociateAll();
+		associations.addAssociation(incorrectSailor06, entity);
+		assertFalse(incorrectSailor06.isOnTheTargetEntity(associations));
+
 		Sailor incorrectSailor07 = new Sailor(7, 2,1,  "sailor7");
+		associations.dissociateAll();
+		associations.addAssociation(incorrectSailor07, entity);
+		assertFalse(incorrectSailor07.isOnTheTargetEntity(associations));
+
 		Sailor incorrectSailor08 = new Sailor(8, 2,2,  "sailor8");
+		associations.dissociateAll();
+		associations.addAssociation(incorrectSailor08, entity);
+		assertFalse(incorrectSailor08.isOnTheTargetEntity(associations));
 	}
 
 }
