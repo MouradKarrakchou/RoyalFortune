b004636396478d6add4bc112fe15f65eb5551126
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-03-22 22:18:03 +0100

    puuuuuuuuuuuush #68

---
+++ player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/Shape/SegmentTest.java
@@ -0,0 +1,67 @@
+package fr.unice.polytech.si3.qgl.royal_fortune.Shape;
+
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.shape.Segment;
+import fr.unice.polytech.si3.qgl.royal_fortune.ship.Position;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SegmentTest {
+    private Segment s;
+    private Position p1;
+    private Position p2;
+
+    @BeforeEach
+    void init(){
+        p1 = new Position(0,0,0);
+        p2 = new Position(50,0,0);
+        s = new Segment(p1, p2);
+    }
+
+    @Test
+    void getPointATest(){
+        assertEquals(p1, s.getPointA());
+    }
+
+    @Test
+    void getPointBTest(){
+        assertEquals(p2, s.getPointB());
+    }
+
+    @Test
+    void getLenghtTest(){
+        assertEquals(50., s.getLength());
+    }
+
+    @Test
+    void getATest(){
+        assertEquals(-0., s.getA());
+    }
+
+    @Test
+    void getBTest(){
+        assertEquals(-0., s.getB());
+    }
+
+    @Test
+    void computeATest(){
+        assertEquals(-0., s.computeA(p1, p2));
+    }
+    @Test
+    void computeBTest(){
+        assertEquals(-0., s.computeB(p1, p2));
+    }
+
+    @Test
+    void computeIntersectionWith_IntersectionTest(){
+        Segment other = new Segment(new Position(25,-25,-Math.PI/2),new Position(25,25,-Math.PI/2));
+        Optional<Position> res = s.computeIntersectionWith(other);
+        //assertTrue(res.isPresent());----------> A VERIFIER
+    }
+
+
+}
