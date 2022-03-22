d32d6b9c52cfe7c1f8d267d9a0c0a97842300c76
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-03-22 17:17:16 +0100

    beginning of the path finding #68

--- player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/PreCalculator.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/PreCalculator.java
@@ -49,13 +49,13 @@ public class PreCalculator {
         double newY = ship.getPosition().getY();
         double angle = ship.getPosition().getOrientation();
 
-        if(ship.getSail().isOpenned()) {
+        /**if(ship.getSail().isOpenned()) {
             Wind theWind = wind;
             double windNorm = theWind.getStrength() * Math.cos(theWind.getOrientation() - angle);
 
             newX += windNorm * Math.cos(angle);
             newY += windNorm * Math.sin(angle);
-        }
+        }**/
 
         while(!seaMap.isInCheckpointShipPos(seaMap.getCurrentFictitiousCheckPoint(), newX, newY) && (numberOfSailors<=sailors.size())){
             norm = 165 * numberOfSailors / (double) ship.getNbrOar();
