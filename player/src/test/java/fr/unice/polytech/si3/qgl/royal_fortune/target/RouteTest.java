8214e9d557a199621a581c47444e6bfa28ab38d1
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-03-23 14:16:34 +0100

    test

--- player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/target/RouteTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/target/RouteTest.java
@@ -1,8 +1,11 @@
 package fr.unice.polytech.si3.qgl.royal_fortune.target;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RouteTest {
+    @BeforeEach
+    void init(){}
     @Test
     void getListSegmentTest(){}
 }
