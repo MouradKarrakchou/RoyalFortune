b8a7604906a7a75beba9eddcfef54a0624365d59
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-03-23 14:20:13 +0100

    test

--- player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/target/RouteTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/target/RouteTest.java
@@ -1,11 +1,28 @@
 package fr.unice.polytech.si3.qgl.royal_fortune.target;
 
+import fr.unice.polytech.si3.qgl.royal_fortune.calculus.Cartologue;
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.Reef;
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.Stream;
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.Wind;
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.shape.Segment;
+import fr.unice.polytech.si3.qgl.royal_fortune.ship.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RouteTest {
     @BeforeEach
-    void init(){}
+    void init(){
+        List<Stream> listStream = new ArrayList<>();
+        List<Reef> listReef = new ArrayList<>();
+        Wind wind = new Wind(0., 5.);
+
+        Segment segment = new Segment(new Position(0,0,0), new Position(10,0,0));
+        Cartologue cartologue = new Cartologue(listStream, listReef, wind);
+        Route r = new Route(segment, cartologue);
+    }
     @Test
     void getListSegmentTest(){}
 }
