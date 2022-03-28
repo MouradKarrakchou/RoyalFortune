aa4a88c469cee70e82a02c8885c6953183e5ea5b
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-03-28 17:11:25 +0200

    Updating Mathematician computeTrajectoryOnBeaconTest #76

--- player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/MathematicianTest.java
+++ player/src/test/java/fr/unice/polytech/si3/qgl/royal_fortune/MathematicianTest.java
@@ -71,16 +71,13 @@ class MathematicianTest {
     void computeTrajectoryOnBeaconTest(){
         List<Beacon> beacons = new ArrayList<>();
 
-        Beacon beacon00 = new Beacon(new Position(50, -50), new Circle());
-        beacons.add(beacon00);
-
         Beacon beacon01 = new Beacon(new Position(0, 0), new Circle());
         beacons.add(beacon01);
 
         Optional<Beacon> bestBeacon = mathematician.computeTrajectory(beacons,
                 new Position(0, 0), new Position(100, 0));
 
-        assertFalse(bestBeacon.isEmpty());
+        assertTrue(bestBeacon.isEmpty());
     }
 
 }
