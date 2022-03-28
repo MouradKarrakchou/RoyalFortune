84d7d7a6dd1fbd0d5b49a3bac6e2fc7a3b34a45d
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-03-28 18:49:59 +0200

    Creating Geometry circle

--- player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/GeometryCircle.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/GeometryCircle.java
@@ -1,24 +1,38 @@
 package fr.unice.polytech.si3.qgl.royal_fortune.calculus;
 
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.shape.Circle;
 import fr.unice.polytech.si3.qgl.royal_fortune.environment.shape.Rectangle;
+import fr.unice.polytech.si3.qgl.royal_fortune.environment.shape.Shape;
 import fr.unice.polytech.si3.qgl.royal_fortune.ship.Position;
+import fr.unice.polytech.si3.qgl.royal_fortune.target.Beacon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GeometryCircle {
+    private final double beaconRadius = 50;
+    private final double securityScaling = 20;
 
     private GeometryCircle() {}
 
-    /**
-     * check if the rectangle is in the circle
-     * @return true if the rectangle is the circle
-     */
-    public static boolean rectangleIsInCircle(Rectangle rectangle, Position currentPos, double radius){
-        List<Position> positionList= GeometryRectangle.computeCorner(currentPos, rectangle);
-        for(Position position:positionList){
-            if(Mathematician.distanceFormula(currentPos,position)<=radius)
-                return true;
-        }
-        return false;
+    public List<Beacon> generateBeacon(Position sheepPosition, Position checkpointPosition, Position reefPosition, Circle reefShape){
+        List<Beacon> beaconList = new ArrayList<>();
+
+        double vectorSheepCheckpointX = checkpointPosition.getX() - sheepPosition.getX();
+        double vectorSheepCheckpointY = checkpointPosition.getY() - sheepPosition.getY();
+        double normSheepCheckpoint = Math.sqrt(vectorSheepCheckpointX * vectorSheepCheckpointX + vectorSheepCheckpointY * vectorSheepCheckpointY);
+
+        double normalVectorX = -vectorSheepCheckpointY / normSheepCheckpoint;
+        double normalVectorY = vectorSheepCheckpointX / normSheepCheckpoint;
+
+        double firstBeaconX = reefPosition.getX() + normalVectorX * (reefShape.getRadius() + beaconRadius + securityScaling);
+        double firstBeaconY = reefPosition.getY() + normalVectorY * (reefShape.getRadius() + beaconRadius + securityScaling);
+        beaconList.add(new Beacon(new Position(firstBeaconX, firstBeaconY), new Circle(beaconRadius)));
+
+        double secondBeaconX = reefPosition.getX() - normalVectorX * (reefShape.getRadius() + beaconRadius + securityScaling);
+        double secondBeaconY = reefPosition.getY() - normalVectorY * (reefShape.getRadius() + beaconRadius + securityScaling);
+        beaconList.add(new Beacon(new Position(secondBeaconX, secondBeaconY), new Circle(beaconRadius)));
+
+        return beaconList;
     }
 }
