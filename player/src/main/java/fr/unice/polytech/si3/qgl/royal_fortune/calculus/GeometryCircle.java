29e5b067aff34e254e46d758d2337cba8bf943ad
AyoubIMAMI <youbima17@gmail.com>
2022-03-28 18:50:18 +0200

    #76 #83

--- player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/GeometryCircle.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/GeometryCircle.java
@@ -15,6 +15,19 @@ public class GeometryCircle {
 
     private GeometryCircle() {}
 
+    /**
+     * check if the rectangle is in the circle
+     * @return true if the rectangle is the circle
+     */
+    public static boolean rectangleIsInCircle(Rectangle rectangle, Position currentPos, double radius){
+        List<Position> positionList= GeometryRectangle.computeCorner(currentPos, rectangle);
+        for(Position position:positionList){
+            if(Mathematician.distanceFormula(currentPos,position)<=radius)
+                return true;
+        }
+        return false;
+    }
+
     public List<Beacon> generateBeacon(Position sheepPosition, Position checkpointPosition, Position reefPosition, Circle reefShape){
         List<Beacon> beaconList = new ArrayList<>();
 
