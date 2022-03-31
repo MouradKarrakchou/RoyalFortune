3bd038cf2ce0e2b40ca555825e71ebdae47e603e
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-03-31 09:50:28 +0200

    Creating Dijkstra algorithm class #86

---
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/Dijkstra.java
@@ -0,0 +1,17 @@
+package fr.unice.polytech.si3.qgl.royal_fortune.calculus;
+
+import fr.unice.polytech.si3.qgl.royal_fortune.target.Beacon;
+
+import java.util.List;
+
+public class Dijkstra {
+    /**
+     * For a given list of checkpoints, will proceed the Dijkstra algorithm to find the shortest path.
+     * @param cartologue - The class providing the list of reef.
+     * @param beacons - The list of all the beacons on the map.
+     * @return The shortest path as an array of Beacon.
+     */
+    public static List<Beacon> proceedDijkstra(Cartologue cartologue, List <Beacon> beacons){
+        return null;
+    }
+}
