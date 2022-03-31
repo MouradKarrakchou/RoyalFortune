8be7f094c547fe4ae91c8398cf6855100e30e431
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-03-31 23:54:55 +0200

    Adding javadoc #86

--- player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/dijkstra/Dijkstra.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/dijkstra/Dijkstra.java
@@ -10,12 +10,12 @@ import java.util.*;
 
 public class Dijkstra {
     /**
-     * For a given list of checkpoints, will proceed the Dijkstra algorithm to find the shortest path.
+     * For a given list of Beacons, will proceed the Dijkstra algorithm to find the shortest path possible.
      * @param departure - The position of the departure point.
      * @param arrival - The position of the arrival point.
      * @param cartologue - The class providing the list of reef.
      * @param beacons - The list of all the beacons on the map.
-     * @return The shortest path as an array of Beacon.
+     * @return The shortest path as a stack of Beacons (departure & arrival positions are excluded).
      */
     public static Stack<Beacon> proceedDijkstra(Position departure, Position arrival, Cartologue cartologue, List <Beacon> beacons){
         List<DijkstraNode> availableNodes = generateDijkstraNodes(beacons);
@@ -41,21 +41,21 @@ public class Dijkstra {
      * is lower than the previous one.
      *
      * @param currentNode - The node to update all the available neighbor nodes.
-     * @param availableNodes - The list of all nodes available (nodes we don't already go through).
+     * @param availableNodes - The list of all available nodes (nodes we don't already go through).
      * @param cartologue - The tool used to calculate the new node value.
-     * @return The list of all updated nodes
+     * @return The set of all updated nodes.
      */
     private static Set<DijkstraNode> updateNodes(DijkstraNode currentNode, List<DijkstraNode> availableNodes, Cartologue cartologue){
-        // Creating a list of all updated nodes.
+        // Creating a set of all updated nodes.
         availableNodes.remove(currentNode);
         Set<DijkstraNode> updatedNodes = new HashSet<>();
 
         for (DijkstraNode node : availableNodes){
-            // Creating a segment between the currentNode and one of the possible node.
+            // Creating a route between the currentNode and one of the possible node.
             Segment segment = new Segment(currentNode.getNode().getPosition(), node.getNode().getPosition());
             Route route = new Route(segment, cartologue);
 
-            // If the calculated path length is shorter than the older one, we update the node.
+            // If the calculated path is shorter than the older one, we update the node.
             double newNodeValue = route.getValue() + currentNode.getNodeValue();
             if(newNodeValue < node.getNodeValue()){
                 node.setNodeValue(newNodeValue);
@@ -66,10 +66,9 @@ public class Dijkstra {
         return updatedNodes;
     }
 
-
     /**
-     * For each beacon on the beacon list will generate a list of node containing the beacon.
-     * @param beacons - The provides list of beacons.
+     * For each beacon on the beacon list, we will generate a list of node containing the beacon.
+     * @param beacons - The provided list of beacons.
      * @return The generated list of nodes.
      */
     private static List<DijkstraNode> generateDijkstraNodes(List<Beacon> beacons){
--- player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/dijkstra/DijkstraNode.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/calculus/dijkstra/DijkstraNode.java
@@ -11,8 +11,10 @@ public class DijkstraNode implements Comparable<DijkstraNode> {
     private double nodeValue;
 
     /**
-     * @param nodeObject - The object situated on the node
-     *                    * A Position for the checkpoint & the ship
+     * Create a DijkstraNode with a given IPositionable nodeObject.
+     * Previous node is null and node value is Infinite.
+     * @param nodeObject - The IPositionable object situated on the node, can be :
+     *                    * A Position for the checkpoint & the ship,
      *                    * A Beacon fot the beacons.
      */
     public DijkstraNode(IPositionable nodeObject){
@@ -21,14 +23,30 @@ public class DijkstraNode implements Comparable<DijkstraNode> {
         this.nodeValue = Double.POSITIVE_INFINITY;
     }
 
-    private DijkstraNode(IPositionable currentNode, double nodeValue){
-        this.nodeObject = currentNode;
+    /**
+     * Create a DijkstraNode with a given IPositionable nodeObject and a node value.
+     * Previous node is null.
+     * @param nodeObject - The IPositionable object situated on the node, can be :
+     *                    * A Position for the checkpoint & the ship,
+     *                    * A Beacon fot the beacons.
+     * @param nodeValue - The value to associate to the DijkstraNode.
+     */
+    private DijkstraNode(IPositionable nodeObject, double nodeValue){
+        this.nodeObject = nodeObject;
         this.previousNode = null;
         this.nodeValue = nodeValue;
     }
 
-    public static DijkstraNode generateStartingNode(IPositionable currentNode){
-        return new DijkstraNode(currentNode, 0);
+    /**
+     * Generate a StartingNode.
+     * A starting node is characterized by having its node value set to 0.
+     * @param nodeObject - The IPositionable object situated on the node, can be :
+     *                    * A Position for the checkpoint & the ship,
+     *                    * A Beacon fot the beacons.
+     * @return The StartingNode containing an IPositionable object and a node value of 0.
+     */
+    public static DijkstraNode generateStartingNode(IPositionable nodeObject){
+        return new DijkstraNode(nodeObject, 0);
     }
 
     public void setPreviousNode(DijkstraNode previousNode){
@@ -48,7 +66,8 @@ public class DijkstraNode implements Comparable<DijkstraNode> {
     }
 
     /**
-     * @return The shortest path to access this Node.
+     * For a given DijkstraNode, returns all the Beacons that must be traversed to reach it.
+     * @return The Beacons that must be traversed to reach the given DijkstraNode.
      */
     public Stack<Beacon> getPath(){
         return getPath(new Stack<>(), this);
@@ -56,10 +75,10 @@ public class DijkstraNode implements Comparable<DijkstraNode> {
 
     /**
      * RECURSIVE
-     * For a given Path return
+     * For a given DijkstraNode, returns all the Beacons that must be traversed to reach it.
      * @param currentPath The current path already calculates.
-     * @param currentNode - The node to retrieve the path
-     * @return The shortest path to access the given node.
+     * @param currentNode - The DijkstraNode to retrieve the path.
+     * @return The Beacons that must be traversed to reach the given DijkstraNode.
      */
     private Stack<Beacon> getPath(Stack<Beacon> currentPath, DijkstraNode currentNode){
         if (currentNode == null)
@@ -71,7 +90,6 @@ public class DijkstraNode implements Comparable<DijkstraNode> {
 
         return(getPath(currentPath, currentNode.previousNode));
     }
-
 
     @Override
     public int compareTo(DijkstraNode node) {
