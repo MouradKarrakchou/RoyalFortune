5e3e095c7e362086e0cefc9d405f4ea5e13f5afe
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-04-19 18:06:45 +0200

    Fixing polygon beacon placement#97

--- player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/environment/shape/Polygone.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/environment/shape/Polygone.java
@@ -73,7 +73,7 @@ public class Polygone extends Shape{
 
     @Override
     public List<Beacon> generateBeacon(Position aPosition, boolean isAReef) {
-        return GeometryPolygone.generateBeacon(this);
+        return GeometryPolygone.generateBeacon(aPosition, this);
     }
 
     @Override
