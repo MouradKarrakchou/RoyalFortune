35323c840d60a0379d909c6c6f361f65b06e571a
Kilian Bonnet <kilian.bonnet1@etu.univ-cotedazur.fr>
2022-04-19 17:06:36 +0200

    Pour mon gars sur Mourad les Polygones #97

--- player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/environment/shape/Polygone.java
+++ player/src/main/java/fr/unice/polytech/si3/qgl/royal_fortune/environment/shape/Polygone.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import fr.unice.polytech.si3.qgl.royal_fortune.calculus.GeometryPolygone;
 import fr.unice.polytech.si3.qgl.royal_fortune.calculus.GeometryRectangle;
+import fr.unice.polytech.si3.qgl.royal_fortune.calculus.Vector;
 import fr.unice.polytech.si3.qgl.royal_fortune.dao.NextRoundDAO;
 import fr.unice.polytech.si3.qgl.royal_fortune.ship.Position;
 import fr.unice.polytech.si3.qgl.royal_fortune.target.Beacon;
@@ -15,6 +16,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static fr.unice.polytech.si3.qgl.royal_fortune.Cockpit.SECURITY_UPSCALE;
 
 @JsonIgnoreProperties(value = {
         "type"
@@ -45,10 +48,22 @@ public class Polygone extends Shape{
         return segmentList;
     }
 
-    public void updatePolygone(Position position){
-        //Kilian
-        //à faire
+    /**
+     * For a given polygon will shift all local coordinates to their global coordinates and upscale their size
+     * to prevent from oaring to close from a rift.
+     * @param center The center of the shape.
+     */
+    public void updatePolygone(Position center){
+        for(int i = 0; i < vertices.length; i++){
+            Point currentPont = vertices[i];
+            Vector centerPointUnitVector = new Vector(new Point(0,0), currentPont).unitVector();
+            vertices[i] = new Point(
+                    (int) Math.ceil(currentPont.getX() + centerPointUnitVector.x * SECURITY_UPSCALE),
+                    (int) Math.ceil(currentPont.getY() + centerPointUnitVector.y * SECURITY_UPSCALE)
+            );
+        }
     }
+
     @Override
     public List<Beacon> generateBeacon(Position aPosition, boolean isAReef) {
         return GeometryPolygone.generateBeacon(aPosition,this,isAReef);
